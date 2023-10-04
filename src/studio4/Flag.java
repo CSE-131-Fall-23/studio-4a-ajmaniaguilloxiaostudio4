3b3ba6b2a3687b3dc5445f83f85cc6e139ff5d3a
mariaelisaaguilo <mariaelisaaguilo@localhost>
2023-10-04 15:38:42 -0500

    Marialand flag :)

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,17 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(StdDraw.CYAN);
+		StdDraw.filledRectangle(0.5, 0.5, 0.35, 0.2);
+		
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(0.5, 0.35, 0.35, 0.1);
+		
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledCircle(0.5, 0.5, 0.13);
+		
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.text(0.5, 0.5, "Marialand");
+		
 	}
 }
